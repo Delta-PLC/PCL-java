76f629797673fe8581a1208d8ea05442be008f25
Jigeesha Gadge <jigeeshagadge208@gmail.com>
2022-06-10 18:05:17 +0530

    10 June: json read api

--- src/main/java/com/plc/PlcApplication.java
+++ src/main/java/com/plc/PlcApplication.java
@@ -1,16 +1,24 @@
 package com.plc;
 
+import com.plc.user.entity.Role;
+import com.plc.user.entity.Roles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class PlcApplication {
+public class PlcApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlcApplication.class, args);
 	}
+
 
 }
