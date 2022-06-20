02da548dd2036db16fb0aa5c5a8077f0b7c48246
NIRAVENDLOS <nirav@endlos.in>
2022-06-20 11:22:04 +0530

    crossorigin

--- src/main/java/com/plc/user/entity/User.java
+++ src/main/java/com/plc/user/entity/User.java
@@ -36,7 +36,7 @@ public class User{
     }
 
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles_data",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
