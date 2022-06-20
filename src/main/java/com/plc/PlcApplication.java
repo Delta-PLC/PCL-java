33eb1dc58c86eb03498878c443550b5a24675245
NIRAVENDLOS <nirav@endlos.in>
2022-06-20 12:22:35 +0530

    roles add

--- src/main/java/com/plc/PlcApplication.java
+++ src/main/java/com/plc/PlcApplication.java
@@ -48,14 +48,23 @@ public class PlcApplication  implements ApplicationRunner {
 		user.setAddress("karol bag");
 		user.setCity("ahmedabad");
 
-		Role roles=new Role(Roles.ROLE_ADMIN);
 		Role roleSuperadmin=new Role(Roles.ROLE_SUPERADMIN);
+		Role roles=new Role(Roles.ROLE_ADMIN);
+		Role roles1=new Role(Roles.ROLE_EMPLOYEE);
+		Role roles2=new Role(Roles.ROLE_MODERATOR);
+		Role roles3=new Role(Roles.ROLE_USER);
+
 
 
 		List<User> data=userRepository.findAll();
 		if (data.isEmpty()) {
-			roleRepository.save(roles);
 			roleRepository.save(roleSuperadmin);
+			roleRepository.save(roles);
+			roleRepository.save(roles1);
+			roleRepository.save(roles2);
+			roleRepository.save(roles3);
+
+
 			user.getRoles().add(roleSuperadmin);
 			user.getRoles().add(roles);
 		//	log.info("{}",user.getRoles());
