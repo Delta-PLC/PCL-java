4b87007eba772d463d953edc15064971346f0a2e
Jigeesha Gadge <jigeeshagadge208@gmail.com>
2022-07-04 17:51:16 +0530

    commit: 04/07/22

--- src/main/java/com/plc/panel/Controller/PanelController.java
+++ src/main/java/com/plc/panel/Controller/PanelController.java
@@ -41,7 +41,7 @@ public class PanelController {
 
         Connection con= DriverManager.getConnection(url, user, password);
         //String select_sql= "select * from public.tblmachine_details;";
-        String select_sql= "select tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port, company_details.company_name,tblplc_customer.reg_name, tbl_add_panel_with_register_tag.register_tag, addres_regisater_type.address from tblmachine_details left join company_details on tblmachine_details.c_id=company_details.company_id left join tblplc_customer on tblmachine_details.machine_id =tblplc_customer.m_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id";
+        String select_sql= "select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port, company_details.company_name,tblplc_customer.reg_name, tbl_add_panel_with_register_tag.register_tag, addres_regisater_type.address,tbl_plcreg_type.plc_register from tblmachine_details left join company_details on tblmachine_details.c_id=company_details.company_id left join tblplc_customer on tblmachine_details.machine_id =tblplc_customer.m_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id";
         PreparedStatement pstn = con.prepareStatement(select_sql);
         ResultSet rs = pstn.executeQuery();
         int i=1;
@@ -51,6 +51,7 @@ public class PanelController {
             while (rs.next()) {
 
 
+
                 int dev_id=rs.getInt("dev_id");
                 int machine_port=rs.getInt("machine_port");
                 String machine_ip=rs.getString("machine_ip");
@@ -58,33 +59,29 @@ public class PanelController {
                 String reg_name=rs.getString("reg_name");
                 String register_tag=rs.getString("register_tag");
                 int address=rs.getInt("address");
-
-
+                String plc_register=rs.getString("plc_register");
 
                 i++;
+
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("IP",machine_ip);
                 jsonObject.put("unitid",dev_id);
                 jsonObject.put("port",machine_port);
-               // jsonObject.put("company",company_name);
-               // jsonObject.put("method",reg_name);
-               // jsonObject.put("lport",register_tag);
-               // jsonObject.put("2222",address);
-
-
-                JSONArray work = new JSONArray();
+                List<String> listStrings = new ArrayList<>();
+                listStrings.add(reg_name);
+                listStrings.add(plc_register);
+                listStrings.add(register_tag);
                 JSONObject jsonObject1 = new JSONObject();
-                jsonObject1.put("method",reg_name);
-                jsonObject1.put("lport",register_tag);
-                jsonObject1.put("2222",address);
-                work.add(jsonObject1);
-
-               //jsonObject.put("Work", work);
-
-                obj.add(work);
+                jsonObject1.put("Method",listStrings);
+                List<String> listStrings1 = new ArrayList<>();
+                listStrings1.add(String.valueOf(address));
+                JSONObject jsonObject2 = new JSONObject();
+                jsonObject1.put("lport",listStrings1);
+                JSONArray obj1 = new JSONArray();
+                obj1.add(jsonObject1);
+                jsonObject.put("work",obj1);
+                jsonObject.put("company",company_name);
                 obj.add(jsonObject);
-
-
             }
             file.write(String.valueOf(obj));
             file.close();
@@ -101,20 +98,27 @@ public class PanelController {
     @PutMapping(value = "/{machineId}")
     public ResponseEntity<?> updateMachine(@PathVariable Long machineId, @RequestBody PanelSaveDto panelSaveDto) throws SQLException {
 
-        Object updateMachine=machineServiceImpl.update(machineId, panelSaveDto);
+        Object updateMachine = machineServiceImpl.update(machineId, panelSaveDto);
 
         String url = "jdbc:postgresql://localhost:5432/plc_project";
         String user = "postgres";
         String password = "postgres";
-        int i=1;
-        Connection con= DriverManager.getConnection(url, user, password);
-        //System.out.println("connection done");
-//        String select_sql= "select * from public.tblmachine_details;";
-       // String select_sql= "select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port, company_details.company_name,tblplc_customer.reg_name, tbl_add_panel_with_register_tag.register_tag, addres_regisater_type.address from tblmachine_details left join company_details on tblmachine_details.c_id=company_details.company_id left join tblplc_customer on tblmachine_details.machine_id =tblplc_customer.m_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id where tblmachine_details.machine_id='"+i+"'";
-        String select_sql= "select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port, company_details.company_name,tblplc_customer.reg_name, tbl_add_panel_with_register_tag.register_tag, addres_regisater_type.address,tbl_plcreg_type.plc_register from tblmachine_details left join company_details on tblmachine_details.c_id=company_details.company_id left join tblplc_customer on tblmachine_details.machine_id =tblplc_customer.m_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id";
+        int i = 1;
+        Connection con = DriverManager.getConnection(url, user, password);
+
+//        String sql2 = "SELECT machine_id FROM public.tblmachine_details ORDER BY machine_id DESC limit 1";
+//        PreparedStatement pstn2 = con.prepareStatement(sql2);
+//        ResultSet rs2 = pstn2.executeQuery();
+//        while (rs2.next()) {
+////            int k = rs2.getInt("machine_id");
+//            int k=1;
+//            System.out.println("last idex is : " + k);
+//        //}
+
+//        String select_sql = "select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port, company_details.company_name,tblplc_customer.reg_name, tbl_add_panel_with_register_tag.register_tag, addres_regisater_type.address,tbl_plcreg_type.plc_register from tblmachine_details left join company_details on tblmachine_details.c_id=company_details.company_id left join tblplc_customer on tblmachine_details.machine_id =tblplc_customer.m_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id where machine_id='"+k+"'"  ;
+        String select_sql = "select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port, company_details.company_name,tblplc_customer.reg_name, tbl_add_panel_with_register_tag.register_tag, addres_regisater_type.address,tbl_plcreg_type.plc_register from tblmachine_details left join company_details on tblmachine_details.c_id=company_details.company_id left join tblplc_customer on tblmachine_details.machine_id =tblplc_customer.m_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id";
         PreparedStatement pstn = con.prepareStatement(select_sql);
         ResultSet rs = pstn.executeQuery();
-
 
 
         try {
@@ -125,25 +129,22 @@ public class PanelController {
 
                 //data from tblmachine_details
 
-                int dev_id=rs.getInt("dev_id");
-                int machine_port=rs.getInt("machine_port");
-                String machine_ip=rs.getString("machine_ip");
-                String company_name=rs.getString("company_name");
+                int dev_id = rs.getInt("dev_id");
+                int machine_port = rs.getInt("machine_port");
+                String machine_ip = rs.getString("machine_ip");
+                String company_name = rs.getString("company_name");
 
 
-
-                String reg_name=rs.getString("reg_name");
-                String register_tag=rs.getString("register_tag");
-                int address=rs.getInt("address");
-                String plc_register=rs.getString("plc_register");
-
+                String reg_name = rs.getString("reg_name");
+                String register_tag = rs.getString("register_tag");
+                int address = rs.getInt("address");
+                String plc_register = rs.getString("plc_register");
 
 
                 JSONObject jsonObject = new JSONObject();
-                jsonObject.put("IP",machine_ip);
-                jsonObject.put("unitid",dev_id);
-                jsonObject.put("port",machine_port);
-
+                jsonObject.put("IP", machine_ip);
+                jsonObject.put("unitid", dev_id);
+                    jsonObject.put("port", machine_port);
 
 
                 List<String> listStrings = new ArrayList<>();
@@ -153,23 +154,24 @@ public class PanelController {
                 //listStrings.add(String.valueOf(address));
 
                 JSONObject jsonObject1 = new JSONObject();
-                jsonObject1.put("Method",listStrings);
+                jsonObject1.put("Method", listStrings);
 
                 List<String> listStrings1 = new ArrayList<>();
                 listStrings1.add(String.valueOf(address));
                 JSONObject jsonObject2 = new JSONObject();
-                jsonObject2.put("lport",listStrings1);
+                jsonObject1.put("lport", listStrings1);
 
                 JSONArray obj1 = new JSONArray();
                 obj1.add(jsonObject1);
-                obj1.add(jsonObject2);
+                //obj1.add(jsonObject2);
 
-                jsonObject.put("work",obj1);
-                jsonObject.put("company",company_name);
+                jsonObject.put("work", obj1);
+                jsonObject.put("company", company_name);
 
                 obj.add(jsonObject);
 
                 i++;
+
             }
 
             file.write(String.valueOf(obj));
@@ -180,15 +182,20 @@ public class PanelController {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+//            k--;
+//    }
 
 //        {
-//            "machineName": "Machine2",
-//                "machineIp": "192.168.14.2",
-//                "machinePort": 502,
-//                "devId": 2,
-//                "permission": "write",
-//                "active": false
-//        }
+//    "machineName":"machine1",
+//    "machineIp":"10.60.82.01",
+//    "machinePort":1,
+//    "devId":1,
+//    "permissionn":"read",
+//    "machineActive":true,
+//    "companyEntityList": {
+//            "company_id": 1
+//    }
+//}
 
         return new ResponseEntity<>(PageResponse.SuccessResponse(updateMachine),HttpStatus.OK);
 
