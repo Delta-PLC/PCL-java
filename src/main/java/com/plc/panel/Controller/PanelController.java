22c08c18721280e277978bbb8ef86421c8e6eff5
nirv-ptel <niravkothiya2@gmail.com>
2022-07-11 18:55:40 +0530

    putmapping:11/7/22

--- src/main/java/com/plc/panel/Controller/PanelController.java
+++ src/main/java/com/plc/panel/Controller/PanelController.java
@@ -36,97 +36,15 @@ public class PanelController {
     public ResponseEntity<?> saveMachine(@RequestBody PanelSaveDto panelSaveDto) throws SQLException {
         Object machineData=machineServiceImpl.save(panelSaveDto);
 
-
-        String url = "jdbc:postgresql://localhost:5432/plc_project";
-        String user = "postgres";
-        String password = "postgres";
-
-        Connection con= DriverManager.getConnection(url, user, password);
-        String select_sql="select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port,tbl_plcreg_type.plc_register from tblmachine_details left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id";
-        PreparedStatement pstn = con.prepareStatement(select_sql);
-        ResultSet rs = pstn.executeQuery();
-        int i=1;
-        try {
-            JSONArray obj = new JSONArray();
-            FileWriter file = new FileWriter("/home/endloss/Desktop/machine.json");
-            while (rs.next()) {
-
-                int machine_id = rs.getInt("machine_id");
-                String machine_ip = rs.getString("machine_ip");
-                int dev_id = rs.getInt("dev_id");
-                int machine_port = rs.getInt("machine_port");
-                String plc_register = rs.getString("plc_register");
-                //  String company_name = rs.getString("company_name");
-                //int address = rs.getInt("address");
-                //String plc_name = rs.getString("plc_name");
-                //String plc_mode = rs.getString("plc_mode");
-                //String reg_name = rs.getString("reg_name");
-                //String register_tag = rs.getString("register_tag");
-                //String plc_register = rs.getString("plc_register");
-
-                i++;
-
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put("IP",machine_ip);
-                jsonObject.put("unitid",dev_id);
-                jsonObject.put("port",machine_port);
-
-                JSONObject jsonObject1 = new JSONObject();
-                JSONArray obj1 = new JSONArray();
-
-                List<String> listStrings = new ArrayList<>();
-                listStrings.add(plc_register);
-
-                jsonObject1.put("Method",listStrings);
-                List<String> listStrings1 = new ArrayList<>();
-                String select_sql1="select addres_regisater_type.address from addres_regisater_type left join tblmachine_details on addres_regisater_type.company_plc_id=tblmachine_details.c_id where machine_id='"+machine_id+"'";
-                PreparedStatement pstn1 = con.prepareStatement(select_sql1);
-                ResultSet rs1 = pstn1.executeQuery();
-                while (rs1.next())
-                {
-                    int address = rs1.getInt("address");
-                    listStrings1.add(String.valueOf(address));
-                }
-                //rs1.close();
-                jsonObject1.put("lport",listStrings1);
-
-                obj1.add(jsonObject1);
-                jsonObject.put("work",obj1);
-               // jsonObject.put("company",company_name);
-                obj.add(jsonObject);
-               // System.out.println("json array:"+obj);
-
-            }
-            file.write(String.valueOf(obj));
-            file.close();
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-
         log.info("machine save Data =>{} ",machineData);
-        return new ResponseEntity<>(PageResponse.SuccessResponse(machineData), HttpStatus.CREATED);
-    }
-    @PutMapping(value = "/{machineId}")
-    public ResponseEntity<?> updateMachine(@PathVariable Long machineId, @RequestBody PanelSaveDto panelSaveDto) throws SQLException {
 
-        Object updateMachine = machineServiceImpl.update(machineId, panelSaveDto);
 
         String url = "jdbc:postgresql://localhost:5432/plc_project";
         String user = "postgres";
         String password = "postgres";
-        int i = 1;
-        Connection con = DriverManager.getConnection(url, user, password);
-
-        //String select_sql="select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port,addres_regisater_type.address,plc_company.plc_name,plc_company.plc_mode,tbl_add_panel_with_register_tag.register_tag, tbl_plcreg_type.plc_register from tblmachine_details left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id";
-        //String select_sql = "select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port, company_details.company_name,tblplc_customer.reg_name, tbl_add_panel_with_register_tag.register_tag, addres_regisater_type.address,tbl_plcreg_type.plc_register from tblmachine_details left join company_details on tblmachine_details.c_id=company_details.company_id left join tblplc_customer on tblmachine_details.machine_id =tblplc_customer.m_id left join tbl_add_panel_with_register_tag on tblmachine_details.machine_id=tbl_add_panel_with_register_tag.penal_id left join plc_company on tblmachine_details.plc_id_=plc_company.plc_company_id left join addres_regisater_type on tblmachine_details.plc_id_=addres_regisater_type.company_plc_id left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id";
-
-        String select_sql="select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port,tbl_plcreg_type.plc_register from tblmachine_details left join tbl_plcreg_type on tblmachine_details.plc_id_=tbl_plcreg_type.register_plc_id ";
+        Connection con= DriverManager.getConnection(url, user, password);
+        int i=1;
+        String select_sql="select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port from tblmachine_details";
         PreparedStatement pstn = con.prepareStatement(select_sql);
         ResultSet rs = pstn.executeQuery();
 
@@ -134,47 +52,56 @@ public class PanelController {
         try {
             JSONArray obj = new JSONArray();
 
-            FileWriter file = new FileWriter("/home/endloss/Desktop/machine.json");
+            FileWriter file = new FileWriter("C:/Users/Endlos/Desktop/machine.json");
             while (rs.next()) {
 
                 int machine_id = rs.getInt("machine_id");
                 String machine_ip = rs.getString("machine_ip");
+                System.out.println("----------------------:"+machine_ip);
                 int dev_id = rs.getInt("dev_id");
                 int machine_port = rs.getInt("machine_port");
-                String plc_register = rs.getString("plc_register");
-                System.out.println("----------------------:"+plc_register);
+
+
 
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("IP", machine_ip);
                 jsonObject.put("unitid", String.valueOf(dev_id));
                 jsonObject.put("port", String.valueOf(machine_port));
-                JSONObject jsonObject1 = new JSONObject();
+
                 JSONArray obj1 = new JSONArray();
 
-                List<String> listStrings = new ArrayList<>();
-                listStrings.add(plc_register);
-                jsonObject1.put("Method",listStrings);
-                System.out.println("=====================:"+listStrings);
 
-                List<String> listStrings1 = new ArrayList<>();
-                String select_sql1="select addres_regisater_type.address from addres_regisater_type left join tblmachine_details on addres_regisater_type.company_plc_id=tblmachine_details.c_id where machine_id='"+machine_id+"'";
-                PreparedStatement pstn1 = con.prepareStatement(select_sql1);
-                ResultSet rs1 = pstn1.executeQuery();
-                while (rs1.next())
-                {
-                    int address = rs1.getInt("address");
-                    System.out.println("address: "+address);
-                    listStrings1.add(String.valueOf(address));
+                String sql = "select addres_regisater_type.address , addres_regisater_type.add_reg_id,tbl_add_panel_with_register_tag.add_reg_id from addres_regisater_type left join tbl_add_panel_with_register_tag on addres_regisater_type.add_reg_id=tbl_add_panel_with_register_tag.add_reg_id where penal_id='" + machine_id + "'";
+                PreparedStatement pstnsql = con.prepareStatement(sql);
+                ResultSet rssql = pstnsql.executeQuery();
+                while (rssql.next()) {
+
+                    JSONObject jsonObject1 = new JSONObject();
+                    String address = rssql.getString("address");
+                    String sql1 = "select addres_regisater_type.address,tbl_plcreg_type.plc_register from addres_regisater_type left join tbl_plcreg_type on addres_regisater_type.reg_id=tbl_plcreg_type.register_plc_id where address='" + address + "'";
+                    PreparedStatement pstnsql1 = con.prepareStatement(sql1);
+                    ResultSet rssql1 = pstnsql1.executeQuery();
+                    while (rssql1.next()) {
+                        List<String> listStrings = new ArrayList<>();
+                        List<String> listStrings1 = new ArrayList<>();
+
+                        String plc_register = rssql1.getString("plc_register");
+                        System.out.println("plc_register: " + plc_register);
+
+                        String address1 = rssql1.getString("address");
+                        System.out.println("address1: " + address1);
+
+                        listStrings.add(plc_register);
+                        jsonObject1.put("Method", listStrings);
+                        listStrings1.add(address1);
+                        jsonObject1.put("lport", listStrings1);
+                    }
+                    obj1.add(jsonObject1);
+                    System.out.println("obj1: "+obj1);
                 }
-                //rs1.close();
-                jsonObject1.put("lport",listStrings1);
-
-                obj1.add(jsonObject1);
                 jsonObject.put("work",obj1);
-                // jsonObject.put("company",company_name);
                 obj.add(jsonObject);
-                // System.out.println("json array:"+obj);
-
+                System.out.println("obj: "+obj);
             }
 
             file.write(String.valueOf(obj));
@@ -186,28 +113,87 @@ public class PanelController {
             throw new RuntimeException(e);
         }
 
-//        {
-//    "machineName":"machine1",
-//    "machineIp":"10.60.82.01",
-//    "machinePort":1,
-//    "devId":1,
-//    "permissionn":"read",
-//    "machineActive":true,
-//    "companyEntityList": {
-//            "company_id": 1
-//    }
-//}
-//{
-//    "machineName":"machine47",
-//   "machineIp":"10.60.82.47",
-//    "machinePort":47,
-//   "devId":47,
-//    "permissionn":"write",
-//    "machineActive":true,
-//   "companyEntityList": {
-//            "company_id": 1
-//    }
-//}
+
+//        log.info("machine save Data =>{} ",machineData);
+        return new ResponseEntity<>(PageResponse.SuccessResponse(machineData), HttpStatus.CREATED);
+    }
+    @PutMapping(value = "/{machineId}")
+    public ResponseEntity<?> updateMachine(@PathVariable Long machineId, @RequestBody PanelSaveDto panelSaveDto) throws SQLException, IOException {
+
+        Object updateMachine = machineServiceImpl.update(machineId, panelSaveDto);
+
+        String url = "jdbc:postgresql://localhost:5432/plc_project";
+        String user = "postgres";
+        String password = "postgres";
+        int i = 1;
+        Connection con = DriverManager.getConnection(url, user, password);
+        String select_sql="select tblmachine_details.machine_id,tblmachine_details.machine_ip, tblmachine_details.dev_id, tblmachine_details.machine_port from tblmachine_details";
+        PreparedStatement pstn = con.prepareStatement(select_sql);
+        ResultSet rs = pstn.executeQuery();
+
+
+        try {
+            JSONArray obj = new JSONArray();
+
+            FileWriter file = new FileWriter("C:/Users/Endlos/Desktop/machine.json");
+            while (rs.next()) {
+
+                int machine_id = rs.getInt("machine_id");
+                String machine_ip = rs.getString("machine_ip");
+                System.out.println("----------------------:"+machine_ip);
+                int dev_id = rs.getInt("dev_id");
+                int machine_port = rs.getInt("machine_port");
+
+
+
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("IP", machine_ip);
+                jsonObject.put("unitid", String.valueOf(dev_id));
+                jsonObject.put("port", String.valueOf(machine_port));
+
+                JSONArray obj1 = new JSONArray();
+
+
+                    String sql = "select addres_regisater_type.address , addres_regisater_type.add_reg_id,tbl_add_panel_with_register_tag.add_reg_id from addres_regisater_type left join tbl_add_panel_with_register_tag on addres_regisater_type.add_reg_id=tbl_add_panel_with_register_tag.add_reg_id where penal_id='" + machine_id + "'";
+                    PreparedStatement pstnsql = con.prepareStatement(sql);
+                    ResultSet rssql = pstnsql.executeQuery();
+                    while (rssql.next()) {
+
+                        JSONObject jsonObject1 = new JSONObject();
+                        String address = rssql.getString("address");
+                        String sql1 = "select addres_regisater_type.address,tbl_plcreg_type.plc_register from addres_regisater_type left join tbl_plcreg_type on addres_regisater_type.reg_id=tbl_plcreg_type.register_plc_id where address='" + address + "'";
+                        PreparedStatement pstnsql1 = con.prepareStatement(sql1);
+                        ResultSet rssql1 = pstnsql1.executeQuery();
+                        while (rssql1.next()) {
+                            List<String> listStrings = new ArrayList<>();
+                            List<String> listStrings1 = new ArrayList<>();
+
+                            String plc_register = rssql1.getString("plc_register");
+                            System.out.println("plc_register: " + plc_register);
+
+                            String address1 = rssql1.getString("address");
+                            System.out.println("address1: " + address1);
+
+                            listStrings.add(plc_register);
+                            jsonObject1.put("Method", listStrings);
+                            listStrings1.add(address1);
+                            jsonObject1.put("lport", listStrings1);
+                        }
+                        obj1.add(jsonObject1);
+                    }
+                   jsonObject.put("work",obj1);
+                 obj.add(jsonObject);
+            }
+
+            file.write(String.valueOf(obj));
+            file.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
         return new ResponseEntity<>(PageResponse.SuccessResponse(updateMachine),HttpStatus.OK);
 
